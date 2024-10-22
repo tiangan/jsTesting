21d3141f40c57e3d3931d18e84ab3808bff8423d
tiangan <tian.gan.solo@gmail.com>
2024-10-22 15:45:15 -0400

    add follower count file

--- GroceryTracker/groceryTracker.js
+++ GroceryTracker/groceryTracker.js
@@ -5,4 +5,4 @@ function calculateAmount() {
     grocery3 = parseFloat(document.getElementById('grocery3').value);
     let total = grocery1 + grocery2 + grocery3;
     document.getElementById('result').innerText = `The total amount is : $ ${total}`;
-}
+} 
