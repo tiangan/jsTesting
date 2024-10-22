e5cf79a546da8caf908b56acb63205c0e8773ddb
tiangan <tian.gan.solo@gmail.com>
2024-10-22 15:17:00 -0400

    add grocery tracker file

---
+++ GroceryTracker/groceryTracker.js
@@ -0,0 +1,8 @@
+let grocery1, grocery2, grocery3;
+function calculateAmount() {
+    grocery1 = parseFloat(document.getElementById('grocery1').value);
+    grocery2 = parseFloat(document.getElementById('grocery2').value);
+    grocery3 = parseFloat(document.getElementById('grocery3').value);
+    let total = grocery1 + grocery2 + grocery3;
+    document.getElementById('result').innerText = `The total amount is : $ ${total}`;
+}
