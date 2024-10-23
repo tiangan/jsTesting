4bbf321151b87a8ae59a84cf90b47f822a69628b
tiangan <tian.gan.solo@gmail.com>
2024-10-22 22:00:00 -0400

    add menu price

--- RestaurantMenu/restaurant_menu.js
+++ RestaurantMenu/restaurant_menu.js
@@ -1,6 +1,6 @@
-const breakfastMenu = ['Pancakes', 'Eggs Benedict', 'Oatmeal', 'Frittata'];
-const mainCourseMenu = ['Steak', 'Pasta', 'Burger', 'Salmon'];
-const dessertMenu = ['Cake', 'Ice Cream', 'Pudding', 'Fruit Salad'];
+const breakfastMenu = ['Pancakes - $12', 'Eggs Benedict - $18', 'Oatmeal - $8', 'Frittata - $15'];
+const mainCourseMenu = ['Steak - $22', 'Pasta - $19', 'Burger - $19', 'Salmon - $25'];
+const dessertMenu = ['Cake - $12', 'Ice Cream - $10', 'Pudding - $8', 'Fruit Salad - $12'];
 
 const breakfastMenuItemsHTML = breakfastMenu.map((item, index) => `<p>Item ${index + 1}: ${item}</p>`).join('');
 document.getElementById('breakfastMenuItems').innerHTML = breakfastMenuItemsHTML;
